80c6d1d3cf0db2b7c090571528f198b2a22c9450
CodyCline <clinecody98@gmail.com>
2020-08-11 15:46:49 -0700

    feat: Add webauthn functions, account create and read methods

--- models/accounts.go
+++ models/accounts.go
@@ -1,22 +1,74 @@
 package models
 
 import (
-	"encoding/json"
+	"crypto/rand"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+//Account A simple user account with authenticators
 type Account struct {
 	gorm.Model
+	ID          string
+	Username    string `json:"username"` //Username or email
 	Name        string `json:"name"`
-	DisplayName string `json:"displayName"`
-	credentials []Credential
+	Credentials []Credential
 }
 
+//Credential Represents the data from a key in database serialized form
 type Credential struct {
 	gorm.Model
-	AAGUID    []byte `gorm:"size:255"`
-	Details   []byte `gorm:"size:2048"`
-	SignCount uint32
-	FKName    string
+	AAGUID     []byte `gorm:"size:255"`
+	Details    []byte `gorm:"size:2048"`
+	SignCount  uint32
+	FKUsername string
+}
+
+//NewUser Create a new user in the database
+func NewUser(username string, name string) *Account {
+	user := &Account{}
+	user.ID = randomID()
+	user.Username = name
+	user.Name = username
+	GetDB().Create(user)
+	return user
+}
+
+//GetUser Get a user from the database using their username
+func GetUser(name string) (*Account, error) {
+	account := &Account{}
+	GetDB().Table("accounts").Where("name = ?", name).First(account)
+	if account.Username == "" {
+		return account, fmt.Errorf("error getting user '%s': does not exist", name)
+	}
+	return account, nil
+}
+
+func randomID() string {
+	buf := make([]byte, 8)
+	rand.Read(buf)
+	return string(buf[:])
+}
+
+//Webauthn Methods
+
+//WebAuthnID Get webauthn id
+func (a Account) WebAuthnID() []byte {
+	return []byte(a.ID)
+}
+
+//WebAuthnName Return the users username such as email address
+func (a Account) WebAuthnName() string {
+	return a.Username
+}
+
+//WebAuthnDisplayName Return the user's displayed name
+func (a Account) WebAuthnDisplayName() string {
+	return a.Name
+}
+
+// WebAuthnIcon is not (yet) implemented
+func (a Account) WebAuthnIcon() string {
+	return ""
 }
--- models/setup.go
+++ models/setup.go
@@ -1,9 +1,9 @@
 package models
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"log"
 )
 
 //DB Declare database
@@ -13,7 +13,6 @@ func init() {
 	database, err := gorm.Open("sqlite3", "test.db")
 
 	if err != nil {
-		log.Fatal("Failed to connect to database!")
 		panic("Check if test.db file exists!")
 	}
 	database.AutoMigrate(Account{}, Credential{})
